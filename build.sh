e4fb4b599b9b37a2748d7cd7492e834b8ab15364
Kaiede <user@biticus.net>
2021-12-13 10:54:25 -0800

    Add comment about QEMU_CPU use

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@
 TAG=${1:-dev}
 COMMIT=${2:-main}
 
+# Build
+#
+# QEMU_CPU needs to be set to max when building on Apple Silicon.
+# Cuts down on illegal instruction errors.
 docker build . \
     --platform linux/amd64 \
     -t kaiede/minecraft-bedrock-backup:${TAG} \
