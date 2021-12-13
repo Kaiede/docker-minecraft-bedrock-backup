6024c0e33a737ef6c03cee0e8a00112b2dcc1acc
Kaiede <user@biticus.net>
2021-12-13 07:05:38 -0800

    Update for Building on M1

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ TAG=${1:-dev}
 COMMIT=${2:-main}
 
 docker build . \
+    --platform linux/amd64 \
     -t kaiede/minecraft-bedrock-backup:${TAG} \
     --build-arg CACHEBUST=$(date +%s) \
     --build-arg COMMIT=${COMMIT}
