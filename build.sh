cc82f971b74595e553bdfc3c5ec640702de24041
Kaiede <user@biticus.net>
2021-12-13 10:44:36 -0800

    Make M1 Builds More Reliable

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ COMMIT=${2:-main}
 docker build . \
     --platform linux/amd64 \
     -t kaiede/minecraft-bedrock-backup:${TAG} \
+    --build-arg QEMU_CPU=max \
     --build-arg CACHEBUST=$(date +%s) \
     --build-arg COMMIT=${COMMIT}
