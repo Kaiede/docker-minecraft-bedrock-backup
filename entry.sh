7d18be1cf69f933547e4d14e740eaa4421e261c2
Kaiede <user@biticus.net>
2021-12-19 12:05:21 -0800

    Add Logging Options to Entrypoint

--- entry.sh
+++ entry.sh
@@ -6,16 +6,33 @@
 
 set -uo pipefail
 
-if [ "${DEBUG:-false}" == "true" ]; then
-  set -x
-fi
+# Configure extra logging
+LOGGING_ARGS=""
+case "${LOGGING:-normal}" in
+  "trace")
+    LOGGING_ARGS="--trace"
+    set -x
+    ;;
 
-: "${DOCKER_PATH:=/usr/bin/docker}"
+  "debug")
+    LOGGING_ARGS="--debug"
+    set -x
+    ;;
+esac
+
+# Configure arguments for service
+DOCKER_PATH="/usr/bin/docker"
+: "${BACKUP_INTERVAL:=${INTERVAL_SEC:-24h}}"
 : "${DATA_DIR:=/backups}"
 : "${CONFIG_FILE:=config.yml}"
-: "${BACKUP_INTERVAL:=${INTERVAL_SEC:-24h}}"
 
-# TODO: Handle backwards compat here looking for a config.json and/or yaml by default.
+# Check for new config file first, but fall back
+# onto the old default if it doesn't exist.
+if [ ! -e "${DATA_DIR}/${CONFIG_FILE}" ]; then
+  echo "WARN[0000] ${CONFIG_FILE} not found, falling back to config.json"
+  CONFIG_FILE="config.json"
+fi
 
 # Fire backup via BedrockifierCLI Tool
-/opt/bedrock/bedrockifierd "${DATA_DIR}/${CONFIG_FILE}" --docker-path "${DOCKER_PATH}" --backup-path "${DATA_DIR}" >&2
+echo RUN [0000] /opt/bedrock/bedrockifierd "${DATA_DIR}/${CONFIG_FILE}" --docker-path "${DOCKER_PATH}" --backup-path "${DATA_DIR}" ${LOGGING_ARGS}
+/opt/bedrock/bedrockifierd "${DATA_DIR}/${CONFIG_FILE}" --docker-path "${DOCKER_PATH}" --backup-path "${DATA_DIR}" ${LOGGING_ARGS} >&2
