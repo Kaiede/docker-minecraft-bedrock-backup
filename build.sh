4ee1415b4edfb5979d808d87e16d44b11648440e
Kaiede <user@biticus.net>
2021-12-21 17:21:38 -0800

    More tweaks around the template and build architecture

--- build.sh
+++ build.sh
@@ -4,54 +4,28 @@
 
 set -euo pipefail
 
-TAG=${1:-dev}
+tag=${1:-dev}
 COMMIT=${2:-main}
 PUSH=${3:-nopush}
+dockerRepo=kaiede/minecraft-bedrock-backup
+dockerBaseTag=$dockerRepo:${tag}
 
-baseimage_arm64="swiftarm/swift:5.5.2-ubuntu-21.04"
-baseimage_amd64="swift:5.5.2"
-
-render() {
-    sedStr="
-    s!%%BASE_IMAGE%%!$image!g;
-    "
-
-    sed -r "$sedStr" $1
-}
-
-ARCHES=(arm64 amd64)
-for ARCH in ${ARCHES[*]}; do
-    echo Building: $ARCH
-    if [ -e Dockerfile ]; then
-        rm Dockerfile
-    fi
-
-    image=baseimage_$ARCH
-    eval image=\${$image}
-    echo Using Image: $image
-    render Dockerfile.template > Dockerfile
-
-    docker build . \
-        --platform=linux/${ARCH} \
-        -t kaiede/minecraft-bedrock-backup:${ARCH}-${TAG} \
-        --build-arg QEMU_CPU=max \
-        --build-arg CACHEBUST=$(date +%s) \
-        --build-arg COMMIT=${COMMIT} \
-        --build-arg ARCH=${ARCH}
-
-    if [ "$PUSH" == "push" ]; then
-        docker push kaiede/minecraft-bedrock-backup:${ARCH}-${TAG}
-    fi
-done
-
-if [ "$PUSH" == "push" ]; then
-    docker manifest rm \
-        kaiede/minecraft-bedrock-backup:${TAG}
-
-    docker manifest create \
-        kaiede/minecraft-bedrock-backup:${TAG} \
-        --amend kaiede/minecraft-bedrock-backup:arm64-${TAG} \
-        --amend kaiede/minecraft-bedrock-backup:amd64-${TAG} 
-
-    docker manifest push kaiede/minecraft-bedrock-backup:${TAG}
+arch=`arch`
+if [ "$arch" == "x86_64" ]; then
+    arch=amd64
 fi
+if [ "$arch" == "aarch64" ]; then
+    arch=arm64
+fi
+
+. render.sh $arch
+
+dockerTag=$dockerRepo:${arch}-${tag}
+dockerfile=$arch.dockerfile
+
+docker build . -f $dockerfile \
+    -t $dockerTag \
+    --build-arg QEMU_CPU=max \
+    --build-arg CACHEBUST=$(date +%s) \
+    --build-arg COMMIT=${COMMIT} \
+    --build-arg ARCH=${arch}
